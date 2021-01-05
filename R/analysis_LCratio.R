ee16b7a83301ed3bafdb5245627716d90326e981
Shun Hasegawa <snwa0003@SLUmc0247.local>
2021-01-05 10:44:34 +0100

    added analysis on G/S ratio

--- R/analysis_LCratio.R
+++ R/analysis_LCratio.R
@@ -1,7 +1,10 @@
 LCratio_d <- bind_rows(pyr_L, pyr_FH) %>% 
-  mutate(LCratio = (`G-lignin` + `S-lignin` + Phenol)/Carbohydrate) %>% # lignin:carbohydrate
+  mutate(LCratio = (`G-lignin` + `S-lignin` + Phenol)/Carbohydrate,  # lignin:carbohydrate
+         GSratio =  `G-lignin` / `S-lignin`,) %>%                    # G/S-lignin ratios
   left_join(soil_CN_d) 
 
+
+# Lignin:Carbohydrate -----------------------------------------------------
 
 lc_leafd15N_m1 <- lmer(log(LCratio) ~ leaf_d15N * horizon + (1|id), LCratio_d)
 Anova(lc_leafd15N_m1, test.statistic = "F")
@@ -13,7 +16,7 @@ lc_leafd15N_m2 <- update(lc_leafd15N_m1, subset=-c(11, 39))
 Anova(lc_leafd15N_m2, test.statistic = "F")
 plot(lc_leafd15N_m2)
 qqPlot(resid(lc_leafd15N_m2))
-# interactions were due to the Outliers
+# interactions were due to the outliers
 
 # try transformation
 lc_leafd15N_m3 <- lmer(1/LCratio ~ leaf_d15N * horizon + (1|id), LCratio_d)
@@ -29,3 +32,13 @@ lc_leafd15N_fm1 <- lmer(1/LCratio ~ leaf_d15N * horizon + (1|id),
 Anova(lc_leafd15N_fm1, test.statistic = "F")
 plot(lc_leafd15N_fm1)
 qqPlot(resid(lc_leafd15N_fm1))
+
+
+
+
+# G:S-lignin --------------------------------------------------------------
+gs_leafd15N_m1 <- lmer(log(GSratio) ~ leaf_d15N * horizon + (1|id), LCratio_d)
+summary(gs_leafd15N_m1)
+Anova(gs_leafd15N_m1, test.statistic = "F")
+plot(gs_leafd15N_m1)
+qqPlot(resid(gs_leafd15N_m1))
